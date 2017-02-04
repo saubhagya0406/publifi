1cd7748864dde8d22866b8247399e5032f17edc6
saubhagya <saubhagyakumarsingh@gmail.com>
2017-02-03 21:22:31 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+// Start session
+session_start();
+
+// Check if session is going on
+if(isset($_SESSION['validUser'])){
+    header("Location: http://localhost/publify/home.php");
+}
+
+// Setting message
+$msg="";
+if(isset($_SESSION['LoginErrorMsg'])){
+    $msg=$_SESSION['LoginErrorMsg'];
+    session_unset();
+    session_destroy();
+}
+if($msg==""){
+    $msg="Login Page";
+}
+?>
+<html>
+    <head>
+      <link rel="stylesheet" type="text/css" href="css/login.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="form_bg">
+                    <form action="php/login.php" method="post">
+                        <h2 class="text-center"><?php echo $msg; ?></h2>
+                        Email ID<br/>
+                        <div class="form-group">
+                            <input type="email" class="form-control" name="eid">
+                        </div>
+                        Password<br/>
+                        <div class="form-group">
+                            <input type="password" class="form-control" name="pswd">
+                        </div>
+                        <br/>
+                        <div class="align-center">
+                            <button type="submit" class="btn btn-default" id="login" name="submit">Login</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
